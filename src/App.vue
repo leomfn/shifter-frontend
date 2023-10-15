d3607934bbea911181bf79029bdaf656b82fec42
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-10-15 22:46:20 +0200

    init vue app

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import axios from 'axios';
+
+console.log('hello world');
+const items = [{ test: 1 }]
+
+</script>
+
+<template>
+  hello world
+  <button v-for="item in items" :key="item.test">{{ item.test }}</button>
+</template>
+
+<style ></style>
