0796e307d9ec788da23d09c66aa4a9243697e391
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-12-02 03:24:14 +0100

    resize and restyle login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -28,52 +28,62 @@ const submitLogin = async () => {
 </script>
 
 <template>
-    <div class="container">
-        <div class="notification title is-primary">
-            Login
-        </div>
+    <div class="hero is-full-height">
 
-        <form @submit.prevent="submitLogin">
-            <div class="container box">
-                <div class="field">
-                    <label class="label">
-                        Username
-                    </label>
-                    <div class="control has-icons-left has-icons-right">
-                        <input class="input is-success" type="text" placeholder="Enter Username" v-model="username" />
-                        <span class="icon is-small is-left">
-                            <i class="bi bi-person"></i>
-                        </span>
-                        <span class="icon is-small is-right">
-                            <i class="bi bi-check"></i>
-                        </span>
-                    </div>
-                    <p class="help is-success">This username is available</p>
-                </div>
+        <div class="hero-body">
+            <div class="columns is-centered">
+                <div class="box">
+
+                    <div class="notification subtitle">Login to <strong>⚙️ shifter</strong></div>
+
+                    <form @submit.prevent="submitLogin">
+                        <div class="field">
+                            <label class="label">
+                                Username
+                            </label>
+                            <div class="control has-icons-left has-icons-right">
+                                <input class="input" type="text" placeholder="Enter Username" v-model="username" />
+                                <span class="icon is-small is-left">
+                                    <i class="bi bi-person"></i>
+                                </span>
+                                <!-- <span class="icon is-small is-right">
+                                    <i class="bi bi-check"></i>
+                                </span> -->
+                            </div>
+                            <!-- <p class="help is-success">This username is available</p> -->
+                        </div>
 
 
-                <div class="field">
-                    <label class="label">
-                        Password
-                    </label>
-                    <div class="control has-icons-left has-icons-right">
-                        <input class="input is-danger" type="password" placeholder="Enter Password" v-model="password" />
-                        <span class="icon is-small is-left">
-                            <i class="bi bi-key"></i>
-                        </span>
-                        <span class="icon is-small is-right">
-                            <i class="bi bi-x"></i>
-                        </span>
-                    </div>
-                    <p class="help is-danger">Password invalid</p>
-                </div>
+                        <div class="field">
+                            <label class="label">
+                                Password
+                            </label>
+                            <div class="control has-icons-left has-icons-right">
+                                <input class="input" type="password" placeholder="Enter Password" v-model="password" />
+                                <span class="icon is-small is-left">
+                                    <i class="bi bi-key"></i>
+                                </span>
+                                <!-- <span class="icon is-small is-right">
+                                    <i class="bi bi-x"></i>
+                                </span> -->
+                            </div>
+                            <!-- <p class="help is-danger">Password invalid</p> -->
+                        </div>
 
-                <div class="control">
-                    <button class="button is-primary">
-                        Submit
-                    </button>
+                        <div class="control">
+                            <div class="buttons">
+                                <button class="button is-primary">
+                                    Submit
+                                </button>
+                                <div class="button">
+                                    Create Account
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
                 </div>
             </div>
-        </form>
+        </div>
     </div>
 </template>
